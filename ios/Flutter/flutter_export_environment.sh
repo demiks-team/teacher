591a94599a28034ba89d45f5d27bbe0d51616df6
Pouriya <pouriya.dev@outlook.com>
2026-01-29 17:48:19 +0330

    Change icons

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Pouriya\develop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Pouriya\Desktop\New folder\mobileteacherapp"
+export "FLUTTER_APPLICATION_PATH=E:\Demiks\mobileappteacher"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.10"
-export "FLUTTER_BUILD_NUMBER=21"
+export "FLUTTER_BUILD_NAME=1.0.14"
+export "FLUTTER_BUILD_NUMBER=25"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
--- lib/src/shared/theme/colors/app_colors.dart
+++ lib/src/shared/theme/colors/app_colors.dart
@@ -2,11 +2,11 @@
 
 class AppColors {
   // ------------------ Main colors demiks ------------------
-  static const primaryColor = '#067a6c';
-  static const accentColor = '#faa61a';
-  static const backgroundColorMintTulip = '#c1f2e8';
-  static const backgroundColorAlto = "#d3d3d3";
+  static const primaryColor = '#04594f';
+  static const accentColor = '#d97d28';
+  static const backgroundColorMintTulip = '#d4fff5';
+  static const backgroundColorAlto = "#ededed";
   static const backgroundColorGray = "#888888";
-  static const backgroundColorMineShaft = "#333333";
+  static const backgroundColorMineShaft = "#1a1a1a";
   // --------------------------------------------------------
 }
